c4c4065dce95a1112d6a6539e2163ee7fb165203
gaurav <vishal.wedowebapps@gmail.com>
2018-12-12 14:50:37 +0530

    Library intiate
    Logger Library Upload

---
+++ loggerlibrary/src/main/java/app/vishal/com/loggerlibrary/Log.java
@@ -0,0 +1,17 @@
+package app.vishal.com.loggerlibrary;
+
+public class Log
+{
+    public static void i(String tag,String value)
+    {
+
+    }
+    public static void e(String tag,String value)
+    {
+        Log.e(tag,value);
+    }
+    public static void v(String tag,String value)
+    {
+        Log.e(tag,value);
+    }
+}
